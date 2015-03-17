c936eaebc7c73468aff4dff9a8eb03760a1e3aa6
Joey Lawrance <lawrancej@wit.edu>
2015-03-17 10:41:23 -0400

    Starter code.

---
+++ controller.cpp
@@ -0,0 +1,56 @@
+#include "controller.h"
+#include <map>
+
+using namespace std;
+
+Controller::Controller() {
+    model = new Model();
+    view = new View("Game", 1024, 768);
+}
+
+Controller::~Controller() {
+    delete model;
+    delete view;
+}
+/**
+References:
+https://wiki.libsdl.org/SDL_PollEvent
+https://wiki.libsdl.org/SDL_Event
+*/
+void Controller::loop() {
+    SDL_Event e;
+    unsigned int lastTime = 0, currentTime;
+    std::map<SDL_Keycode, Direction> direction;
+    direction[SDLK_UP] = UP;
+    direction[SDLK_DOWN] = DOWN;
+    direction[SDLK_LEFT] = LEFT;
+    direction[SDLK_RIGHT] = RIGHT;
+
+    while(!model->gameOver()) {
+        currentTime = SDL_GetTicks();
+        // Do stuff here to animate as necessary
+        view->show(model);
+        if (SDL_PollEvent(&e) != 0) {
+            switch (e.type) {
+            case SDL_QUIT:
+                return;
+            case SDL_KEYDOWN:
+                switch(e.key.keysym.sym) {
+                case SDLK_DOWN:
+                case SDLK_UP:
+                case SDLK_LEFT:
+                case SDLK_RIGHT:
+//                    model->go(direction[e.key.keysym.sym]);
+                break;
+                default:
+                break;
+                }
+            case SDL_MOUSEBUTTONDOWN:
+                break;
+            }
+        }
+    }
+    // TODO: show something nice?
+    view->show(model);
+    SDL_Delay(3000);
+}
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "controller.h"
+
+int main(int argc, char** argv) {
+    Controller c;
+    c.loop();
+    return 0;
+}
