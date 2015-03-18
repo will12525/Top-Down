50d4b1e894478721014760edee03ae884efb2198
TomPlano <planot@wit.edu>
2015-03-18 13:48:42 -0400

    menu outline

---
+++ menu.cpp
@@ -0,0 +1,46 @@
+class Menu{
+	Menu(){
+		
+	}
+	public:
+	//navigate any menu (wasd)
+	//select (enter)
+		
+};
+
+class Start{
+	Start(){
+		
+	}
+	public:
+	void start(){
+		
+	}
+	void load(){
+		
+	}
+};
+
+class InGame{
+	InGame(){
+		
+	}
+	public:
+	void quit(){
+		
+	}
+	void save(){
+		
+	}
+};
+
+
+
+
+
+
+
+
+
+
+
